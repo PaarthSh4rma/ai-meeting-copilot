1a867c655a66c12c788e1984ad45da86eb565ba1
Paarth Sharma <psha0064@student.monash.edu>
2026-04-25 01:41:30 +1000

    fixing homepage ui

--- backend/app/main.py
+++ backend/app/main.py
@@ -9,6 +9,7 @@ app.add_middleware(
     allow_origins=[
     "http://localhost:3000",
     "https://ai-meeting-copilot-psi.vercel.app",
+    
 ],
     allow_credentials=True,
     allow_methods=["*"],
